5370adee6216a075411803595dc402e2b79125b5
chen_steven <593449501@qq.com>
2019-04-06 18:25:01 +0800

    微服务服务注册中心

---
+++ microsoft-discovery-eureka/src/main/java/com/cxj/eureka/MicrosoftDiscoveryEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.cxj.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosoftDiscoveryEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrosoftDiscoveryEurekaApplication.class, args);
+    }
+
+}
