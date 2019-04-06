caa7b5a4286d36965343fcd00494c5bc4d08f504
chen_steven <593449501@qq.com>
2019-04-06 19:16:05 +0800

    使用apache common fileupload

--- microsoftservice-simple-provider-user/src/main/java/cloud/controller/CommonContoller.java
+++ microsoftservice-simple-provider-user/src/main/java/cloud/controller/CommonContoller.java
@@ -1,5 +1,10 @@
 package cloud.controller;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,10 +12,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.support.StandardMultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -33,7 +40,15 @@ public class CommonContoller {
     public Map<Object, Object> uploadFile(HttpServletRequest request) {
         BufferedReader br = null;
         BufferedWriter bw = null;
-        List<MultipartFile> files = ((MultipartHttpServletRequest)request).getFiles("files");
+        StandardMultipartHttpServletRequest servletRequest = (StandardMultipartHttpServletRequest) request;
+        Iterator<String> iterator = servletRequest.getFileNames();
+        while (iterator.hasNext()){
+            MultipartFile file = servletRequest.getFile(iterator.next());
+            String fileName = file.getOriginalFilename();
+            System.out.println(fileName);
+        }
+
+        /*List<MultipartFile> files = ((MultipartHttpServletRequest)request).getFiles("files");
         for(MultipartFile file : files){
             String fileName = file.getOriginalFilename();
             fileName = fileName.substring(fileName.lastIndexOf("\\") + 1);
@@ -72,7 +87,7 @@ public class CommonContoller {
                     }
                 }
             }
-        }
+        }*/
         return null;
     }
 
